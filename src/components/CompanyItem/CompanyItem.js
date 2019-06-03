744a9f7947912069276a28a79206f80247830406
Laura Jové <kesiah85@gmail.com>
2019-06-03 18:19:05 +0200

    Add company item to list

--- src/components/CompanyItem/CompanyItem.js
+++ src/components/CompanyItem/CompanyItem.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CompanyItem = props => (
-  <div>{props.item.id}</div>
-);
+const CompanyItem = ({ item }) => {
+  const {
+    name, salesRepId1, dateCreated, typeId, address1, address2, countryId,
+  } = item;
+
+  const formatDate = (valueDate) => {
+    const date = new Date(valueDate);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    if (day < 10) day = `0${day}`;
+    if (month < 10) month = `0${month}`;
+
+    return `${day}/${month}/${year}`;
+  };
+
+  const salesResponsible = (salesRepId1) ? salesRepId1.value : '';
+  let address = '';
+  if (address1) address = `${address1}`;
+  if (address2) address = `${address} ${address2}`;
+
+  const country = (countryId) ? countryId.value : '';
+  const typeCompany = (typeId) ? typeId.value : '';
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <div>{salesResponsible}</div>
+      <div>{formatDate(dateCreated)}</div>
+      <div>{typeCompany}</div>
+      <div>{address}</div>
+      <div>{country}</div>
+    </div>
+  );
+};
+
+CompanyItem.propTypes = {
+  item: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    address1: PropTypes.string,
+    address2: PropTypes.string,
+    dateCreated: PropTypes.string,
+    typeId: PropTypes.object,
+    countryId: PropTypes.object,
+    salesRepId1: PropTypes.object,
+  }).isRequired,
+};
 
 export default CompanyItem;
