7f055e3ea206b72c230d610947a7e9f0db50dcd3
Laura Jové <kesiah85@gmail.com>
2019-06-04 10:50:57 +0200

    Add call to create new company

--- src/components/NewCompany/NewCompany.js
+++ src/components/NewCompany/NewCompany.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import { Redirect } from 'react-router-dom';
 import withAuth from '../withAuth';
 import AuthHelperMethods from '../AuthHelperMethods';
 
@@ -25,41 +23,62 @@ class NewCompany extends Component {
     console.log(this.props);
     const Auth = new AuthHelperMethods();
 
-    Auth.getCompaniesType().then((res) => {
-      console.log("REsult getCompaniesTypeList", res);
+    Auth.getCompaniesType().then((result) => {
+      console.log("REsult getCompaniesTypeList", result);
       this.setState({
-        companiesType: res
+        companiesType: result,
       });
     });
   }
 
-  postDataHandler = () => {
+  postDataHandler = (e) => {
+    e.preventDefault();
+
+    const {
+      name, salesRepId1, typeId, address1, city, postcode, phone, email, comment,
+    } = this.state;
+
     const data = {
-      name: this.state.name,
-      salesRepId1: this.state.salesRepId1,
-      typeId: this.state.typeId,
-      address1: this.state.address1,
-      city: this.state.city,
-      postcode: this.state.postcode,
-      phone: this.state.phone,
-      email: this.state.email,
-      comment: this.state.comment,
+      name,
+      salesRepId1: 103,
+      typeId,
+      address1,
+      city,
+      postcode,
+      phone,
+      email,
+      comment,
     };
 
     console.log('postDataHAndler: ', data);
-    // axios.post('/posts', data)
-    //   .then(response => {
-    //     console.log(response);
-    //     this.props.history.replace('/posts');
-    //     // this.setState( { submitted: true } );
-    //   });
+    const Auth = new AuthHelperMethods();
+
+    Auth.fetch('/accounts', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("Result adding new company", res);
+      console.log(this.props);
+      // redirect to list of companies
+      this.setState({ submitted: true });
+
+      setTimeout(() => {
+        this.props.history.replace('/');
+      }, 1000);
+    });
   }
 
   render() {
+    const {
+      companiesType, submitted, name, salesRepId1, typeId,
+      address1, city, postcode, phone, email, comment,
+    } = this.state;
     // let redirect = null;
-    // if (this.state.submitted) {
-    //   redirect = <Redirect to="/posts" />;
-    // }
+    if (submitted) {
+      console.log("submit");
+      // redirect = <Redirect to="/" />;
+      return <p>Empresa añadida correctamente. En breve redirección</p>;
+    }
     return (
       <div className="NewCompany">
         {/* {redirect} */}
@@ -67,86 +86,89 @@ class NewCompany extends Component {
         <form onSubmit={this.postDataHandler}>
           <label htmlFor="name">
             Nombre
-          <input
+            <input
               required
               id="name"
               type="text"
-              value={this.state.name}
+              value={name}
               onChange={event => this.setState({ name: event.target.value })}
             />
           </label>
           <label htmlFor="salesRepId1">
             Responsable
-          <input
+            <input
               required
               id="salesRepId1"
               type="text"
-              value={this.state.salesRepId1}
+              value={salesRepId1}
               onChange={event => this.setState({ salesRepId1: event.target.value })}
             />
           </label>
           <label htmlFor="typeId">
             Tipo de empresa
-          <input
-              required
+            <select
+              name="typeId"
               id="typeId"
-              type="text"
-              value={this.state.typeId}
-              onChange={event => this.setState({ typeId: event.target.value })}
-            />
+              onBlur={event => this.setState({ typeId: parseInt(event.target.value, 10) })}
+            >
+              {companiesType.map(el => (
+                <option key={el.id} value={el.id}>{el.descriptionES}</option>
+              ))}
+            </select>
           </label>
           <label htmlFor="address1">
             Dirección
-          <input
+            <input
               id="address1"
               type="text"
-              value={this.state.address1}
+              value={address1}
               onChange={event => this.setState({ address1: event.target.value })}
             />
           </label>
           <label htmlFor="city">
             Ciudad
-          <input
+            <input
               id="city"
               type="text"
-              value={this.state.city}
+              value={city}
               onChange={event => this.setState({ city: event.target.value })}
             />
           </label>
           <label htmlFor="postcode">
             Código postal
-          <input
+            <input
               id="postcode"
               type="text"
-              value={this.state.postcode}
+              value={postcode}
               onChange={event => this.setState({ postcode: event.target.value })}
             />
           </label>
           <label htmlFor="phone">
             Teléfono
-          <input
+            <input
               id="phone"
               type="text"
-              value={this.state.phone}
+              value={phone}
               onChange={event => this.setState({ phone: event.target.value })}
             />
           </label>
           <label htmlFor="email">
             Correo electrónico de la empresa
-          <input
+            <input
               id="email"
               type="email"
-              value={this.state.email}
+              value={email}
               onChange={event => this.setState({ email: event.target.value })}
             />
           </label>
           <label htmlFor="comment">
             Comentarios
-          <textarea
+            <textarea
               id="comment"
               rows="4"
-              value={this.state.comment}
-              onChange={(event) => this.setState({ comment: event.target.value })} />
+              value={comment}
+              onChange={event => this.setState({ comment: event.target.value })}
+            />
           </label>
           <button type="submit">Enviar</button>
         </form>
